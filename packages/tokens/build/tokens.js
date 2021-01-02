361cb157fbf74f042a096f9c3ca59e79cbd0e0ac
Eric Cheung <eric.cheung@yourservice.today>
2021-01-02 19:15:16 +0000

    chore: add tokens package

---
+++ packages/tokens/build/tokens.js
@@ -0,0 +1,9 @@
+/**
+ * Do not edit directly
+ * Generated on Sat, 02 Jan 2021 19:03:49 GMT
+ */
+
+export const COLOR_CORE_WHITE = "#ffffff";
+export const COLOR_CORE_BLACK = "#111111";
+export const COLOR_CORE_GRAY = "#dddddd";
+export const COLOR_CORE_BLUE = "#0366d6";
